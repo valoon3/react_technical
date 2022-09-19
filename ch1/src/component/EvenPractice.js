1966b3091147f5def8982b78dcf7ac6733a2725e
valoon3 <valoon2@naver.com>
2022-09-19 21:57:54 +0900

    구조 변경

---
+++ ch1/src/component/EvenPractice.js
@@ -0,0 +1,52 @@
+import {Component} from "react";
+
+
+class EventPractice extends Component {
+    state = {
+        message: '',
+    }
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        })
+    }
+
+    handleClick() {
+        alert(this.state.message);
+        this.setState({
+            message: ''
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>이벤트 연습</h1>
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="아무거나 입력해 보세요"
+                    value={this.state.message}
+                    onChange={(e) => {
+                        this.setState({message: e.target.value})
+                    }}
+                />
+                <button onClick={() => {
+                    alert(this.state.message);
+                    this.setState({
+                        message: ''
+                    });
+                }}>확인</button>
+            </div>
+        )
+    }
+}
+
+export default EventPractice;
