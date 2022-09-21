8e78bb70d46abe12c859b73f475137dc05e09799
valoon3 <valoon2@naver.com>
2022-09-21 23:03:30 +0900

    sass 연결

---
+++ ch3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+
+function App() {
+  return (
+      <>
+        <TodoTemplate>
+            <TodoInsert />
+        </TodoTemplate>
+      </>
+  );}
+
+export default App;
---
+++ ch3/tutorial.md
@@ -0,0 +1,2 @@
+아이콘 리스트와 사용법
+https://react-icons.netlify.com/
