688a9f7890e40a33ad07dcdf52debd2567ade966
valoon3 <valoon2@naver.com>
2022-10-05 21:19:02 +0900

    13.5 중첩된 라우트

--- ch4-router-tutorial/src/App.js
+++ ch4-router-tutorial/src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import React from 'react';
 import Profile from "./pages/Profile";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/profiles/:username" element={<Profile />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/articles/:id" element={<Article />} />
       </Routes>
   );
 }
