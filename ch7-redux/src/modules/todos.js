2fa1b6e24bfeda2f84d65dffc4bf472595a3d1f8
valoon3 <valoon2@naver.com>
2022-10-19 23:33:19 +0900

    redux-actions 가독성 향상

--- ch7-redux/src/modules/todos.js
+++ ch7-redux/src/modules/todos.js
@@ -1,4 +1,4 @@
-import { createAction } from 'redux-actions';
+import {createAction, handleActions} from 'redux-actions';
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
 const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
@@ -55,34 +55,34 @@ const initialState = {
     ]
 };
 
-function todos(state = initialState, action) {
-    switch (action.type) {
-        case CHANGE_INPUT:
-            return {
-                ...state,
-                input: action.input
-            };
-        case INSERT:
-            return {
-                ...state,
-                todos: state.todos.concat(action.todo)
-            };
-        case TOGGLE:
-            return {
-                ...state,
-                todos: state.todos.map(todo => {
-                    return todo.id === action.id ? {...todo, done: !todo.done } : todo;
-                })
-            }
-        case REMOVE:
-            return {
-                ...state,
-                todos: state.todos.filter(todo => todo.id !== action.id)
-            };
-        default:
-            return state;
-    }
-}
+// function todos(state = initialState, action) {
+//     switch (action.type) {
+//         case CHANGE_INPUT:
+//             return {
+//                 ...state,
+//                 input: action.input
+//             };
+//         case INSERT:
+//             return {
+//                 ...state,
+//                 todos: state.todos.concat(action.todo)
+//             };
+//         case TOGGLE:
+//             return {
+//                 ...state,
+//                 todos: state.todos.map(todo => {
+//                     return todo.id === action.id ? {...todo, done: !todo.done } : todo;
+//                 })
+//             }
+//         case REMOVE:
+//             return {
+//                 ...state,
+//                 todos: state.todos.filter(todo => todo.id !== action.id)
+//             };
+//         default:
+//             return state;
+//     }
+// }
 
 // insert의 경우 todo 객체를 액션 객체 안에 넣어 주어야 하기 때문에 두 번째 파라미터에 text를 넣으면 todo 객체가 반환되는 함수를 넣어 주었습니다.
 // 나머지 함수에는 text => text 혹은 id => id 와 같은 형태로 파라미터를 그대로 반환하는 함수를 넣었습니다.
@@ -92,6 +92,16 @@ function todos(state = initialState, action) {
 // 액션 생성 함수를 다 작성했으면 handleActions로 리듀서를 재작성해 보겠습니다.
 // createAction으로 만든 액션 생성 함수는 파라미터로 받아 온 값을 객체 안에 넣을 때 원하는 이름으로 넣는 것이 아니라
 // actions.id, action.todo와 같이 action.payload값을 조회하여 업데이트 하도록 구현해 주어야 합니다.
+// { type: MY_ACTION, payload: 'hello world' } 형태
+
+const todos = handleActions({
+    [CHANGE_INPUT]: (state, { payload: input }) => ({...state, input }), // action = { payload:input }
+    [INSERT]: (state, action) => ({
+        ...state, todos: state.todos.concat(action.payload),
+    }),
+    [TOGGLE]: (state, action) => ({ ...state, todos: state.todos.map(todo => todo.id === action.payload ? {...todo, done: !todo.done } : todo) }),
+    [REMOVE]: (state, action) => ({ ...state, todos: state.todos.filter(todo => todo.id !== action.payload) })
+}, initialState);
 
 
 // 액션 생성 함수는 액션에 필여한 추가 데이터를 모두 payload 라는 이름으로 사용하기 때문에 action.id, action.todo를 조회하는 대신, 모두 공통적으로 action.payload 값을 조회하도록 리듀서 구현해 주어야 합니다.
