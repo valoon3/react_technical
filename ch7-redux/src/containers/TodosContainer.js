f7e1bc34cfb4ab8073717054f4d7617cec9d7d4b
valoon3 <valoon2@naver.com>
2022-10-20 23:06:48 +0900

    최종 todosContainer

--- ch7-redux/src/containers/TodosContainer.js
+++ ch7-redux/src/containers/TodosContainer.js
@@ -1,18 +1,27 @@
-import { connect } from 'react-redux';
+import {connect, useDispatch, useSelector} from 'react-redux';
 import {changeInput, insert, remove, toggle} from "../modules/todos";
 import Todos from "../components/Todos";
+import {useCallback} from "react";
+import useActions from "../customHooks/useActions";
 
 
-const TodosContainer = ({
-    input,
-    todos,
-    changeInput,
-    insert,
-    toggle,
-    remove,
-}) => {
+const TodosContainer = () => {
+    const {input, todos} = useSelector(({ todos }) => ({
+        input: todos.input,
+        todos: todos.todos,
+    }));
+
+    // const [onChangeInput, onInsert, onToggle, onRemove] = useActions([])
+
+    const dispatch = useDispatch();
+
+    const onChangeInput = useCallback(input => dispatch(changeInput(input)), [dispatch]);
+    const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+    const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+    const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+
     return(
-        <Todos input={input} todos={todos} onChangeInput={changeInput} onInsert={insert} onToggle={toggle} onRemove={remove} />
+        <Todos input={input} todos={todos} onChangeInput={onChangeInput} onInsert={onInsert} onToggle={onToggle} onRemove={onRemove} />
     );
 };
 
